5b726578f037267b5027d0f3d85bd2712cfd42d0
kazukiUshijima <info@kfs214.net>
2021-04-16 14:41:19 +0900

    Revert "first commit."
    This reverts commit 388a78be2e70c2bb0cb0432e5317bf22eb753777.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-// react
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
-// material UI
-import { CssBaseline, Button } from "@material-ui/core";
-
-// amplify
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-
-const App = () => (
-  <React.Fragment>
-    <CssBaseline />
-    <Button variant="contained" color="primary">
-      Primary
-    </Button>
-  </React.Fragment>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
