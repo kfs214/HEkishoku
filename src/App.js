34561b12316d30494aa1c0c11f9ff11aeb2499bb
kazukiUshijima <info@kfs214.net>
2021-04-19 08:49:11 +0900

    add] theme color added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 // react
 import React from "react";
 
-// material UI
+// material UI and theme
 import { CssBaseline, Button } from "@material-ui/core";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
+import "./amplify.css";
 
 // amplify
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 const App = () => (
-  <React.Fragment>
+  <MuiThemeProvider theme={theme}>
     <CssBaseline />
     <Button variant="contained" color="primary">
       Primary
     </Button>
-  </React.Fragment>
+    <Button variant="contained" color="secondary">
+      Secondary
+    </Button>
+  </MuiThemeProvider>
 );
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#007F89",
+    },
+    secondary: {
+      main: "#7BAA17",
+    },
+  },
+});
+
+export default theme;
