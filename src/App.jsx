38601d5700549c6e65e378794207699bbdc5cd77
kazukiUshijima <info@kfs214.net>
2021-05-26 07:40:21 +0900

    [ add ] apollo client

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+// react
 import { useState, useEffect } from "react";
+
+// material UI
 import { Container, Toolbar } from "@material-ui/core";
-import Amplify, { Auth, Hub } from "aws-amplify";
-import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
+
+// Apollo
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloLink } from "apollo-link";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from "apollo-client";
+
+// amplify
+import Amplify, { Auth, Hub } from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import { createAuthLink } from "aws-appsync-auth-link";
 import awsExports from "./aws-exports";
 
 // components
@@ -12,6 +25,24 @@ import LoggedIn from "./components/LoggedIn";
 import SignUp from "./components/SignUp";
 
 Amplify.configure(awsExports);
+
+const url = awsExports.aws_appsync_graphqlEndpoint;
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    createAuthLink({
+      url,
+      region: awsExports.aws_appsync_region,
+      auth: {
+        type: awsExports.aws_appsync_authenticationType,
+        jwtToken: async () =>
+          (await Auth.currentSession()).getIdToken().getJwtToken()
+      }
+    }),
+    createHttpLink({ uri: url })
+  ]),
+  cache: new InMemoryCache()
+});
 
 const App = () => {
   const [user, updateUser] = useState(null);
@@ -34,15 +65,17 @@ const App = () => {
   }, []);
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <HEAppBar isLoggedIn={!!user} />
-      <Toolbar />
-      <Container>
-        <AmplifyAuthenticator>
-          {user ? <LoggedIn /> : <SignUp />}
-        </AmplifyAuthenticator>
-      </Container>
-    </MuiPickersUtilsProvider>
+    <ApolloProvider client={client}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <HEAppBar isLoggedIn={!!user} />
+        <Toolbar />
+        <Container>
+          <AmplifyAuthenticator>
+            {user ? <LoggedIn /> : <SignUp />}
+          </AmplifyAuthenticator>
+        </Container>
+      </MuiPickersUtilsProvider>
+    </ApolloProvider>
   );
 };
 
