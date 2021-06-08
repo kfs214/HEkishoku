40451054d8ef3783bce638e6e31cd4b9f7b1dc6f
kazukiUshijima <info@kfs214.net>
2021-06-09 08:01:19 +0900

    [ modif ] client setting moved to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,28 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom";
+
+// material UI
 import { CssBaseline } from "@material-ui/core";
 import { MuiThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
+// Apollo
+import { ApolloProvider } from "@apollo/react-hooks";
+import {
+  ApolloLink,
+  createHttpLink,
+  InMemoryCache,
+  ApolloClient
+} from "@apollo/client";
+
+// Amplify
+import Amplify, { Auth } from "aws-amplify";
+import { createAuthLink } from "aws-appsync-auth-link";
+import awsExports from "./aws-exports";
+
+// component and misc
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,11 +30,35 @@ import reportWebVitals from "./reportWebVitals";
 import theme from "./styles/theme";
 import "./styles/amplify.css";
 
+Amplify.configure(awsExports);
+
+const url = awsExports.aws_appsync_graphqlEndpoint;
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    createAuthLink({
+      url,
+      region: awsExports.aws_appsync_region,
+      auth: {
+        type: awsExports.aws_appsync_authenticationType,
+        jwtToken: async () =>
+          (await Auth.currentSession()).getIdToken().getJwtToken()
+      }
+    }),
+    createHttpLink({ uri: url })
+  ]),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <ApolloProvider client={client}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <App />
+        </MuiPickersUtilsProvider>
+      </ApolloProvider>
     </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
