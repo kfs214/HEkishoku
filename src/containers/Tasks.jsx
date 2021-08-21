f0ea104032472630d3ae66465d6f0ccba25f2dde
kazukiUshijima <info@kfs214.net>
2021-08-21 13:57:54 +0900

    [ add ] culculating function

--- src/containers/Tasks.jsx
+++ src/containers/Tasks.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
-// apollo
+// libs
 import { gql, useQuery, useMutation } from "@apollo/client";
+import { addMinutes, isValid, max, min, parseISO } from "date-fns";
 
 // components, graphql, and consts
 import Tasks from "../components/organisms/Tasks";
@@ -9,6 +10,98 @@ import { tasksByStatus } from "../graphql/queries";
 import { createTask, updateTask } from "../graphql/mutations";
 import CONSTS from "../consts";
 import { handleTaskUpdate } from "../utils";
+
+const calcTime = ({
+  estimatedHour,
+  referenceTime,
+  isCalculatingStartedTimes
+}) => {
+  const addedMinutesPerHour = isCalculatingStartedTimes ? -60 : 60;
+  const addedMinutes = addedMinutesPerHour * estimatedHour;
+
+  return addMinutes(referenceTime, addedMinutes);
+};
+
+const getReferenceTime = ({
+  referenceTimes,
+  isCalculatingStartedTimes = false,
+  index
+}) => {
+  const validReferenceTimes = referenceTimes
+    .map((time) => parseISO(time))
+    .filter((time) => isValid(time));
+
+  if (!validReferenceTimes.length) {
+    return isCalculatingStartedTimes || index ? null : new Date();
+  }
+
+  return isCalculatingStartedTimes
+    ? min(validReferenceTimes)
+    : max(validReferenceTimes);
+};
+
+const calcTimes = ({ tasks, isCalculatingStartedTimes = false }) =>
+  tasks
+    .map((task, index) => {
+      const { estimatedHour } = task;
+
+      const resultKey = isCalculatingStartedTimes
+        ? CONSTS.STARTED_BY
+        : CONSTS.ENDED_AT;
+      const referenceKey = isCalculatingStartedTimes
+        ? CONSTS.ENDED_BY
+        : CONSTS.STARTED_AT;
+
+      const referenceTime = getReferenceTime({
+        referenceTimes: [tasks[index - 1]?.[resultKey], task[referenceKey]],
+        isCalculatingStartedTimes,
+        index
+      });
+
+      const calculatedTime = calcTime({
+        estimatedHour,
+        referenceTime,
+        isCalculatingStartedTimes
+      });
+
+      return { ...task, [resultKey]: calculatedTime };
+    })
+    .slice()
+    .reverse();
+
+const getCalculatedTasks = (tasks) => {
+  const tasksWithEndedAt = calcTimes({ tasks });
+  const tasksWithStartedBy = calcTimes({
+    tasks: tasksWithEndedAt,
+    isCalculatingStartedTimes: true
+  });
+
+  const tasksEntries = tasksWithStartedBy.map((task) => {
+    const { id, ...otherProperties } = task;
+
+    return [id, { ...otherProperties }];
+  });
+
+  return Object.fromEntries(tasksEntries);
+};
+
+const getShownTasks = (tasks) => {
+  const tasksSortedByIndex = [...tasks].sort((a, b) => a.index - b.index);
+  const tasksToCalc = tasksSortedByIndex.map((task) => {
+    const { id, startedAt, endedBy, estimatedHour } = task;
+
+    return { id, startedAt, endedBy, estimatedHour };
+  });
+
+  const calculatedTasks = getCalculatedTasks(tasksToCalc);
+  const tasksWithTimes = tasksSortedByIndex.map((task) => {
+    const calculatedTask = calculatedTasks[task.id];
+
+    return { ...task, ...calculatedTask };
+  });
+
+  return tasksWithTimes;
+};
 
 const EnhancedTasks = () => {
   // useState 系
@@ -101,11 +194,9 @@ const EnhancedTasks = () => {
     });
   };
 
-  const tasksSortedByIndex = [...tasks].sort((a, b) => a.index - b.index);
-
   return (
     <Tasks
-      tasks={tasksSortedByIndex}
+      tasks={getShownTasks(tasks)}
       create={handleCreate}
       copy={handleCopy}
       saveOrder={handleSaveOrder}
